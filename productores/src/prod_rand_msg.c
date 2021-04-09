3f0d4291468b33078d3b8f24b6e673fc0f1f4e8f
Juan Pablo Saucedo <saucedojuanpablo@gmail.com>
2021-04-09 11:52:45 -0300

    lista funcion generador de random message

---
+++ productores/src/prod_rand_msg.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define MSG_LENGTH 50
+void gen_rand_msg(char* buff, size_t buf_len);
+
+
+int main (void) {
+    char msg_buf[MSG_LENGTH];
+    memset(msg_buf, '\0', sizeof(msg_buf));
+    gen_rand_msg(msg_buf, sizeof(msg_buf) - 1);
+    msg_buf[MSG_LENGTH] = '\0';
+	return 0;
+}
+
+void gen_rand_msg(char* buff, size_t buf_len)
+{
+	srand((unsigned int)(time(NULL)));
+	char char1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234"
+        "56789/,.-+=~`<>:";
+	for(int index = 0; index < buf_len; index++)
+	{
+		buff[index] = char1[rand() % (sizeof char1 - 1)];
+	}
+}
