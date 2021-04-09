a7db5e4eaff5d58bc719cbf63aeafc533e8e7b01
Juan Pablo Saucedo <saucedojuanpablo@gmail.com>
2021-04-09 12:47:48 -0300

    Funcion para conseguir sys load agregada

---
+++ productores/src/prod_sys_load.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MSG_LENGTH 50
+
+float get_sys_load();
+
+int main()
+{
+  char msg_buf[MSG_LENGTH];
+  memset(msg_buf, '\0', sizeof(msg_buf));
+  sprintf(msg_buf, "%2.3f load avg 1min", get_sys_load());
+  printf("%s\n", msg_buf);
+}
+
+
+float get_sys_load() {
+  float sys_load_1min;
+  FILE *fptr = fopen("/proc/loadavg", "r");
+  if (fptr == NULL) {
+    fprintf(stderr, "Falló al abrir /proc/loadavg");
+    exit(EXIT_FAILURE);
+  }
+
+  if (fscanf(fptr, "%f", &sys_load_1min) != 1)
+  {
+    fprintf(stderr, "fallo al buscar system load");
+    exit(EXIT_FAILURE);
+  }
+
+  return sys_load_1min; 
+
+}
